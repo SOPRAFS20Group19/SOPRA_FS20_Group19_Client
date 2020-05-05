fbc71c544e43e289cbf4970092503d2d1231f08d
Luis Knufinke <luis.knufinke@uzh.ch>
2020-05-05 16:39:57 +0200

    FS20G19-191 added delete button for user's own messages

--- src/views/InformationPage/Message.js
+++ src/views/InformationPage/Message.js
@@ -9,9 +9,12 @@ import FireplaceCircle from "../MapMarkers/FireplaceCircle.png"
 import RecyclingCircle from "../MapMarkers/RecyclingCircle.png"
 import FountainCircle from "../MapMarkers/FountainCircle.png"
 import HeartUnfilled from "../InformationPage/HeartUnfilled.png"
+import TrashCan from "./TrashCan.png"
 import HeartRed from "../InformationPage/HeartRed.png"
 import ListGroupItem from "react-bootstrap/ListGroupItem";
 import {Button} from "../variables/Button";
+import {RoundButton} from "../variables/RoundButton";
+import avatarArray from "../Avatar/AvatarArray";
 
 const Container = styled.div`
   display: grid;
@@ -24,7 +27,7 @@ const Container = styled.div`
 `;
 
 const Username = styled.div`
-  grid-column: 1 / span 2;
+  grid-column: 1;
   grid-row: 1;
   font-weight: bold;
   font-size: 15px;
@@ -47,6 +50,37 @@ const Timestamp = styled.div`
   padding-right: 15px;
 `;
 
+const Delete = styled.div`
+  grid-column: 2;
+  grid-row: 1;
+  padding-right: 15px;
+`;
+
+const TrashButton = styled.button`
+  &:hover {
+    transform: translateY(-2px);
+  }
+  padding: 0px;
+  width: 30px;
+  height: 30px;
+  border: none;
+  border-radius: 100%;
+  background: transparent;
+  transition: all 0.3s ease;
+  margin: -5px;
+`;
+
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const imgStyle = {
+    "height": "10px",
+    "width": "10px"
+};
+
 export default class Message extends React.Component{
     constructor(props) {
         super(props);
@@ -54,12 +88,38 @@ export default class Message extends React.Component{
         };
     }
 
+    async deleteMessage(){
+        try {
+            const url = '/locations/chats/' + this.props.locationId + '/' + this.props.messageId;
+
+            await api.delete(url);
+
+            this.props.getChat();
+        } catch (e) {
+            alert(`Something went wrong while deleting the message: \n${handleError(e)}`);
+        }
+    }
+
+    // DO NOT CHANGE == to ===!!!
     render(){
         return (
             <Container>
-                <Username>{this.props.sender}</Username>
+                <Username>{this.props.senderUsername}</Username>
                 <Timestamp>{this.props.timestamp}</Timestamp>
                 <Text>{this.props.content}</Text>
+                {localStorage.getItem("userId") == this.props.senderId ? (
+                <Delete>
+                    <ButtonContainer>
+                        <TrashButton
+                            width="75%"
+                            onClick={() => {
+                                this.deleteMessage();
+                            }}
+                        >
+                            <img src={TrashCan} style={imgStyle}/>
+                        </TrashButton>
+                    </ButtonContainer>
+                </Delete>) : null}
             </Container>
         )
     }
