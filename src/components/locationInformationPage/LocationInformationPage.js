1c0a9f9513bf99fc3caf304e5514d72fbea55e40
Luis Knufinke <luis.knufinke@uzh.ch>
2020-05-24 10:31:02 +0200

    fixed loading button for mobile on location information page

--- src/components/locationInformationPage/LocationInformationPage.js
+++ src/components/locationInformationPage/LocationInformationPage.js
@@ -57,6 +57,38 @@ const LoadingContainer = styled.div`
   padding-top: 0px;
 `;
 
+const ButtonSpinner = styled.button`
+  &:hover {
+    transform: translateY(-2px);
+  }
+  padding: 6px;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 13px;
+  text-align: center;
+  color: #003068;
+  width: ${props => props.width || null};
+  height: 35px;
+  border: 2px solid #003068;
+  border-radius: 5px;
+  cursor: ${props => (props.disabled ? "default" : "pointer")};
+  opacity: ${props => (props.disabled ? 0.4 : 1)};
+  background: #66A3E0;
+  transition: all 0.3s ease;
+  @media only screen and (max-width: 700px){
+    font-size: 10px;
+    height: 25px;
+    padding: 3px;
+    border: 1.5px solid #003068;
+  }
+  @media only screen and (max-width: 500px){
+    font-size: 10px;
+    height: 25px;
+    padding: 3px;
+    border: 1.5px solid #003068;
+  }
+`;
+
 // This component is responsible for the edit profile page
 class LocationInformationPage extends React.Component {
     constructor(props) {
@@ -89,7 +121,7 @@ class LocationInformationPage extends React.Component {
     render() {
         return (
             <div>
-                {!this.state.locationToBeShown ? (<LoadingContainer><Button variant="primary" disabled>
+                {!this.state.locationToBeShown ? (<LoadingContainer><ButtonSpinner variant="primary" disabled>
                     <Spinner
                         as="span"
                         animation="border"
@@ -98,7 +130,7 @@ class LocationInformationPage extends React.Component {
                         aria-hidden="true"
                     />
                     Loading Location...
-                </Button></LoadingContainer>) : (
+                </ButtonSpinner></LoadingContainer>) : (
                     <div>
                         <SidebarInfoAndAddLocation avatarNr={localStorage.getItem("userAvatar")} column={3}/>
                         <Container>
